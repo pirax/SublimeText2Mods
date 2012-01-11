4034e51ae4426ee2f6f813a747ff1522a8ba3bd9
Pirax <git@pirax.pl>
2012-01-11 22:41:11 +0100

    added syntax highlighting detection by shebang line

--- FindSyntax.py
+++ FindSyntax.py
@@ -14,18 +14,35 @@ class FindSyntax(sublime_plugin.EventListener):
 
         name = os.path.basename(filename)         
 
-        vimregion = view.find('^#\s*vim:\s*ft=', 0)
+        ## Try to find vim file type marker
+        vim_region = view.find('^#\s*vim:\s*ft=', 0)
         
-        if vimregion:
-            vim = view.substr(view.line(vimregion))
+        ## If found, parse it and set syntax highliting
+        if vim_region:
+            vim = view.substr(view.line(vim_region))
             v = re.match(r'#\s*vim:\s*ft=(\w+)', vim)
             
             if v and v.group(1):
                 syntax = v.group(1)                
                 self.set_syntax(view, syntax)
+            
+            return
+        
+        ## Try to fing shebang line
+        shebang = view.substr(view.line(0))
+
+        ## If found, parse it and set syntax highliting
+        if shebang:
+            s = re.match(r'#!\*/?(?:[^/]/)*([^/]+)', shebang)
+
+            if s and s.group(1):
+                syntax = s.group(1) 
+                self.set_syntax(view, syntax)
+            
+            return
 
     def set_syntax(self, view, syntax):
         syntax_file = os.path.join('Packages', syntax, syntax + '.tmLanguage')
-
+        
         if os.path.exists(os.path.join(os.path.dirname(sublime.packages_path()), syntax_file)):
             view.set_syntax_file(syntax_file)
