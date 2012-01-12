f042409e8683b08e9e518af326558eaba00f5426
Pirax <git@pirax.pl>
2012-01-12 10:56:00 +0100

    plugin support for case sensitive systems

--- FindSyntax.py
+++ FindSyntax.py
@@ -6,6 +6,10 @@ import sublime_plugin
 
 
 class FindSyntax(sublime_plugin.EventListener):
+    packages_dir = 'Packages'
+    highlighting_ext = '.tmLanguage'
+
+
     def on_load(self, view):
         filename = view.file_name()
 
@@ -31,12 +35,30 @@ class FindSyntax(sublime_plugin.EventListener):
 
         ## If any language name was found - try to find and set its syntax
         if match and match.group(1):
-            syntax = match.group(1)                
-            self.set_syntax(view, syntax)
+            syntax = match.group(1)        
+            
+            sdir = self.get_syntax_dir(syntax)        
 
-    def set_syntax(self, view, syntax):
-        syntax_file = os.path.join('Packages', syntax, syntax + '.tmLanguage')
-        
+            if sdir:
+                sfile = self.get_syntax_file(sdir)
+
+                if sfile:
+                    self.set_syntax(view, sdir, sfile)
+
+    def set_syntax(self, view, sdir, sfile):
+        syntax_file = os.path.join(self.packages_dir, sdir, sfile)
+
         if os.path.exists(os.path.join(os.path.dirname(sublime.packages_path()), syntax_file)):
             view.set_syntax_file(syntax_file)
+
+    def get_syntax_dir(self, syntax):
+        for f in os.listdir(os.path.join(os.path.dirname(sublime.packages_path()), self.packages_dir)):            
+            if syntax.lower() == f.lower():
+                return f
+
+    def get_syntax_file(self, syntax_dir):
+        for f in os.listdir(os.path.join(os.path.dirname(sublime.packages_path()), self.packages_dir, syntax_dir)):
+            (file_name, file_ext) = os.path.splitext(f)
             
+            if file_ext.lower() == self.highlighting_ext.lower():
+                return f
