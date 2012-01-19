9650899b471e6462d8440230abe733b59a9bf8df
Pirax <git@pirax.pl>
2012-01-19 08:31:27 +0100

    converting windows path separtor to sublime supported

--- FindSyntax.py
+++ FindSyntax.py
@@ -1,5 +1,6 @@
 import os.path
 import re
+import string
 
 import sublime
 import sublime_plugin
@@ -16,16 +17,17 @@ class FindSyntax(sublime_plugin.EventListener):
         if view.is_scratch() or not filename:
             return
 
-        name = os.path.basename(filename)         
+        name    = os.path.basename(filename)
+        match   = 0
 
         ## Try to find vim file type marker
         vim_region = view.find('^#\s*vim:\s*ft=', 0)
-        
+
         ## If found, parse it and get language name
         if vim_region:
             vim = view.substr(view.line(vim_region))
             match = re.match(r'#\s*vim:\s*ft=(\w+)', vim)
-        else:    
+        else:
             ## Try to find shebang line
             shebang = view.substr(view.line(0))
 
@@ -35,9 +37,9 @@ class FindSyntax(sublime_plugin.EventListener):
 
         ## If any language name was found - try to find and set its syntax
         if match and match.group(1):
-            syntax = match.group(1)        
-            
-            sdir = self.get_syntax_dir(syntax)        
+            syntax = match.group(1)
+
+            sdir = self.get_syntax_dir(syntax)
 
             if sdir:
                 sfile = self.get_syntax_file(sdir)
@@ -49,16 +51,19 @@ class FindSyntax(sublime_plugin.EventListener):
         syntax_file = os.path.join(self.packages_dir, sdir, sfile)
 
         if os.path.exists(os.path.join(os.path.dirname(sublime.packages_path()), syntax_file)):
-            view.set_syntax_file(syntax_file)
+            ## Sublime for some reason does not recognize "widnowslike" path separator and requires /
+            sublime_style_syntax_file_path = string.replace(syntax_file, '\\','/')
+
+            view.set_syntax_file(sublime_style_syntax_file_path)
 
     def get_syntax_dir(self, syntax):
-        for f in os.listdir(os.path.join(os.path.dirname(sublime.packages_path()), self.packages_dir)):            
+        for f in os.listdir(os.path.join(os.path.dirname(sublime.packages_path()), self.packages_dir)):
             if syntax.lower() == f.lower():
                 return f
 
     def get_syntax_file(self, syntax_dir):
         for f in os.listdir(os.path.join(os.path.dirname(sublime.packages_path()), self.packages_dir, syntax_dir)):
             (file_name, file_ext) = os.path.splitext(f)
-            
+
             if file_ext.lower() == self.highlighting_ext.lower():
                 return f
